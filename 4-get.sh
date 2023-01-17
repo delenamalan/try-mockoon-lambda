d8a6fa3d192c4505ea0c2cda56ce82a8854c3ec2
Delena Malan <delenamalan@gmail.com>
2023-01-17 10:48:18 +0100

    Echo URL

--- 4-get.sh
+++ 4-get.sh
@@ -3,4 +3,5 @@ set -eo pipefail
 APIID=$(aws cloudformation describe-stack-resource --stack-name nodejs-apig --logical-resource-id api --query 'StackResourceDetail.PhysicalResourceId' --output text)
 REGION=$(aws configure get region)
 
+echo https://$APIID.execute-api.$REGION.amazonaws.com/api/
 curl https://$APIID.execute-api.$REGION.amazonaws.com/api/ -v
